4827a76016f7ef7edbbeccb095562d63eafd5e24
moran <3575331055@qq.com>
2025-06-03 11:01:47 +0800

    加入获取指定排名的公会名的变量，原因是LG原版一直出问题

--- src/main/java/top/molab/minecraft/legendaryGuildRankAddon/papiHolders/RankHolder.java
+++ src/main/java/top/molab/minecraft/legendaryGuildRankAddon/papiHolders/RankHolder.java
@@ -13,15 +13,20 @@ public class RankHolder extends BaseHolder {
     public String Parse(OfflinePlayer player, String[] args){
         if (!args[0].equals("Rank") || args.length < 3) return null;
         List<Guild> guilds = GuildUtils.sortGuilds(args[1]);
-        if (args.length >= 4 && args[3].equals("Value")) {
+        if (args.length >= 4 ) {
             try {
                 int index = Integer.parseInt(args[2]);
-                return guilds.get(index - 1).getGuild();
+                if (args[3].equals("Value")) {
+                    return String.valueOf( guilds.get(index-1).getMoney());
+                }else if(args[3].equals("Name")){
+                    return guilds.get(index-1).getGuild();
+                }else {return null;}
             } catch (NumberFormatException ex) {
                 return "你输入的索引不合法";
             } catch (IndexOutOfBoundsException ex) {
-                return "无公会";
+                return "";
             }
-        }return "";
+        }
+        return null;
     }
 }
